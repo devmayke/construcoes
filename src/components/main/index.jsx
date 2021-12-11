65ef4035fdf2a5d8d0ec0d9e677c3cbf3166e726
Mayke Ken <devmayke@gmail.com>
2021-12-11 20:46:06 -0300

    Estrutura de ícones

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import './main.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPaintRoller, faScrewdriverWrench } from '@fortawesome/free-solid-svg-icons'
+import Icon from '../../styled/icons/Icon'
+
 
 const Main = () => {
   return (
-   <main className='main'>
-     main
-   </main>
+    <main className='main'>
+      <section className='apresentacao'>
+       <article className=''>
+          <Icon><FontAwesomeIcon icon={faPaintRoller} size='2x' /></Icon>
+          <p>Construção e Reforma predial e residencial do início ao fim: alvenaria, elétrica, hidráulica, texturização e pintura</p>
+       </article>
+       <article>
+          <Icon><FontAwesomeIcon icon={faScrewdriverWrench} size='2x' /></Icon>
+          <p>Nosso time profissional é composto por eletricistas, eletrotécnicos, ceramistas, pedreiros e pintores</p>
+       </article>
+      </section>
+    </main>
   )
 }
 
-export  default Main
+export default Main
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../../components/header'
+import Main from '../../components/main'
 
 const Home = () => {
   return (
     <div className="home">
      <Header/>
+     <Main />
     </div>
   )
 }
