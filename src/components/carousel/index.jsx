f5326f8ed828abdad39fd2d3268418e070942839
Mayke Ken <devmayke@gmail.com>
2021-12-11 17:02:20 -0300

    Animação de zomm nas transições de imagem do carousel

--- src/components/carousel/index.jsx
+++ src/components/carousel/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './carousel.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
@@ -6,22 +6,28 @@ import Carousel from 'nuka-carousel';
 
 
 const CarouselComponent = (props) => {
+  const [currentSlide, setCurrentSlide] = useState(0)
   return (
     <Carousel className='carousel'
     wrapAround={true}
-    autoplayInterval={2000}
+    autoplayInterval={3500}
     autoplay={true}  
+    dragging={true}
+    pauseOnHover={true}
     renderCenterLeftControls={({ previousSlide }) => (
       <FontAwesomeIcon  className='previous' onClick={previousSlide} icon={ faChevronLeft }size='5x' />
     )}
     renderCenterRightControls={({ nextSlide }) => (
       <FontAwesomeIcon   className="next" onClick={nextSlide} icon={ faChevronRight } size='5x' />
     )}  
+    renderTopCenterControls={({ currentSlide }) => (
+      <div>{setCurrentSlide(currentSlide)}</div>
+    )}
     >
        {
          props.images.map((el, i)=>{
-           return(
-             <img src={el} key={i}/>
+          return(
+             <img src={el} className={currentSlide == i ? 'animate':''} key={i}/>
            )
          })
        }
