a73c4b4748d4c6a60b42dfb1164cc6a4e15cb2a6
Mayke Ken <devmayke@gmail.com>
2021-12-11 13:59:28 -0300

    Botões do carousel

--- src/components/carousel/index.jsx
+++ src/components/carousel/index.jsx
@@ -1,18 +1,30 @@
 import React from 'react'
+import './carousel.css'
 import Carousel from 'nuka-carousel';
 
 
-const CarouselComponent = () => {
+
+const CarouselComponent = (props) => {
   return (
-    <Carousel 
+    <Carousel className='carousel'
     wrapAround={true}
     autoplayInterval={2000}
-    autoplay={true}    
+    autoplay={true}  
+    renderCenterLeftControls={({ previousSlide }) => (
+      <button className='previous' onClick={previousSlide}>Previous</button>
+    )}
+    renderCenterRightControls={({ nextSlide }) => (
+      <button className="next" onClick={nextSlide}>Next</button>
+    )}  
     >
-       <div>slide</div>
-       <div>slide</div>
-       <div>slide</div>
-       <div>slide</div>
+       {
+         props.images.map((el, i)=>{
+           return(
+             <img src={el} key={i}/>
+           )
+
+         })
+       }
       </Carousel>
   )
 }
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 import './header.css'
 import Carousel from '../carousel'
 
-const Header= () => {
+const Header= (props) => {
+
+const images = [
+  "https://artia.com/wp-content/uploads/2015/07/obras.jpg",
+  "https://constructapp.io/shared/uploads/2017/05/imprevisto-em-obras-2.jpg",
+  "https://ibecensino.org.br/wp-content/uploads/2019/07/04_Planejamento_controle_obras.png",
+  "https://blog.belgobekaert.com.br/wp-content/uploads/2019/09/GettyImages-628578920-1.jpg"
+]
+
   return (
     <header className='header'>
 
@@ -15,7 +23,7 @@ const Header= () => {
           <li>Área do Cliente</li>
         </ul>
       </nav>
-      <Carousel/>
+      <Carousel images={images}/>
 
     </header>
   )
