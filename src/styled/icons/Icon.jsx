94da66461728ee29e4a769d06d351cbc3c21656b
devmayke <74163797+devmayke@users.noreply.github.com>
2021-12-15 17:36:41 -0300

    Mais ajustes de css

--- src/styled/icons/Icon.jsx
+++ src/styled/icons/Icon.jsx
@@ -9,8 +9,13 @@ export default styled.div`
   width:100px;
   height:100px;
   border:2px solid ${props=>props.cor};
+  border:1px solid rgba(255, 255, 255, 0.898);
+/* box-shadow: 0px 0px 8px 3px rgba(255,255,255,.4); */
+/* box-shadow: 0px 0px 16px 4px rgba(255, 165, 0, 0.631), inset 0px 0px 5px 2px rgba(200, 200, 200, 0.6); */
   border-radius:50%; 
-  background-image:url${props=>props.icon}
+  background-image:url${props=>props.icon};
+  color:white;
+
   `
 
 
