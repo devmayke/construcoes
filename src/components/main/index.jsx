fca1af94672e67462a4e6568551f01bf9ee86263
devmayke <74163797+devmayke@users.noreply.github.com>
2021-12-17 19:55:34 -0300

    Section2

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import './main.css'
 
 import Section1 from '../section1'
+import Section2 from '../section2'
 
 
 const Main = () => {
   return (
 
     <main className='main'>
-      <Section1/>     
-      <section style={{ height:'100vh'}}>        
-      </section>
+      <Section1/> 
+      <Section2/>    
     </main>
   )
 }
---
+++ src/components/section2/index.jsx
@@ -0,0 +1,8 @@
+import './section2.css'
+export default function Section2(){
+    return(
+        <section  style={{ height:'100vh'}}>
+
+        </section>
+    )
+}
