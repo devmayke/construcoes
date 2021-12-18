64dac10881b92c18325c712fbc1b1ad6d97f870c
devmayke <74163797+devmayke@users.noreply.github.com>
2021-12-18 16:30:41 -0300

    Estrutura section2

--- src/components/section2/index.jsx
+++ src/components/section2/index.jsx
@@ -1,8 +1,23 @@
 import './section2.css'
 export default function Section2(){
     return(
-        <section  style={{ height:'100vh'}}>
-
+        <section  className='section2' style={{ height:'100vh'}}>
+            <article>
+                <h1>Teste</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pellentesque dapibus purus eget viverra. Quisque non mollis orci, at vehicula dolor. Morbi orci dolor, elementum at felis ut, efficitur tempus leo. Sed eu diam leo. Maecenas commodo neque sed purus posuere pretium. Suspendisse mattis quam tortor, eu lobortis nulla aliquam sit amet. </p>
+            </article>
+            <article>
+                <h1>Teste</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pellentesque dapibus purus eget viverra. Quisque non mollis orci, at vehicula dolor. Morbi orci dolor, elementum at felis ut, efficitur tempus leo. Sed eu diam leo. Maecenas commodo neque sed purus posuere pretium. Suspendisse mattis quam tortor, eu lobortis nulla aliquam sit amet. </p>
+            </article>
+            <article>
+                <h1>Teste</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pellentesque dapibus purus eget viverra. Quisque non mollis orci, at vehicula dolor. Morbi orci dolor, elementum at felis ut, efficitur tempus leo. Sed eu diam leo. Maecenas commodo neque sed purus posuere pretium. Suspendisse mattis quam tortor, eu lobortis nulla aliquam sit amet. </p>
+            </article>
+            <article>
+                <h1>Teste</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pellentesque dapibus purus eget viverra. Quisque non mollis orci, at vehicula dolor. Morbi orci dolor, elementum at felis ut, efficitur tempus leo. Sed eu diam leo. Maecenas commodo neque sed purus posuere pretium. Suspendisse mattis quam tortor, eu lobortis nulla aliquam sit amet. </p>
+            </article>
         </section>
     )
 }
