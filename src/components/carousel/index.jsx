cd50c60d8b246cb8943d0c5323af6c384085e6c3
devmayke <74163797+devmayke@users.noreply.github.com>
2021-12-21 10:17:39 -0300

    Update

--- src/components/carousel/index.jsx
+++ src/components/carousel/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import './carousel.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
