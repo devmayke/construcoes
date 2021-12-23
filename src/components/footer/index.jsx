02e7a4bee9947f4cfc8724fd20f51893dbea9478
devmayke <74163797+devmayke@users.noreply.github.com>
2021-12-23 18:04:09 -0300

    Melhoras no estilo do footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,12 +1,37 @@
 import './footer.css'
+import logo from './Captura-de-tela-2021-12-23-152415.svg'
+import { Link } from 'react-router-dom'
 
-export default function Footer (){
-    return(
+export default function Footer() {
+    return (
         <footer className='footer'>
-            <div className='wrapper-img'>
-                <img src="https://mapa-da-obra-producao.s3.amazonaws.com/wp-content/uploads/2018/11/sondagem-industria.jpg" alt="" />
-            </div>
-            foooterrrrr
+            <section className='logo-footer'>
+
+                <span >
+                    <img src={logo} alt="" />
+
+                </span><h1>Binho Construções</h1>
+
+            </section>
+            <section className='contato-footer'>
+                <details style={{maxWidth:'50%', margin:'40px 0 0 0',padding:'0', color:'white'}}>
+                    <summary style={{padding:"20px 0", cursor:'pointer'}}>Menu</summary>
+                    <ul className='menu-footer-'>
+                        <li><Link to='construcoes'>Construções</Link></li>
+                        <li><Link to='sobre'>Sobre</Link></li>
+                        <li><Link to='contato'>Contato</Link></li>
+                        <li><Link to='cliente'>Portal do Cliente</Link></li>
+                    </ul>    
+
+                </details>
+                
+            </section>
+            <section className='menu-footer'>
+                Contato
+            </section>
+
+
+
         </footer>
     )
 }
