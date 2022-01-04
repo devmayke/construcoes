22083b3334f94fa7b1372e49a7ba705994b285ba
devmayke <74163797+devmayke@users.noreply.github.com>
2022-01-04 17:01:36 -0300

    ajustes de responsividade e ícones no rodapé

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -6,17 +6,20 @@ import 'animate.css';
 import { useEffect, useState } from 'react';
 import facebook from './facebook.png'
 import tiktok from './tik-tok.png'
-import instagram  from './instagram.png'
+import instagram from './instagram.png'
 import whatsapp from './whatsapp.png'
+import telefone from './telephone-call.png'
+import localizacao from './locator.png'
+import gmail from './gmail.png'
 
 export default function Footer() {
     const [wpmode, setWpmode] = useState('https://web.whatsapp.com/send?phone=5548999973102?&text=Olá%20Binho%20Construções')
-    useEffect(()=>{
+    useEffect(() => {
         console.log(navigator.userAgent.match(/iPad/i))
-        if(navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/Android/i) ){
+        if (navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/Android/i)) {
             setWpmode("https://wa.me/5548999973102?&text=Olá%20Binho%20Construções")
-           }       
-    },[])
+        }
+    }, [])
     return (
         <footer className='footer'>
             <section className='logo-footer'>
@@ -28,8 +31,8 @@ export default function Footer() {
 
             </section>
             <section className='contato-footer'>
-                <details style={{ color:'white'}}>
-                    <summary style={{padding:"20px 0", cursor:'pointer'}}>Páginas</summary>
+                <details style={{ color: 'white' }}>
+                    <summary style={{ padding: "20px 0", cursor: 'pointer' }}>Páginas</summary>
                     <ul className='menu-footer-'>
                         <li><Link to='construcoes'>Construções</Link></li>
                         <li><Link to='sobre'>Sobre</Link></li>
@@ -38,35 +41,37 @@ export default function Footer() {
                     </ul>
                 </details>
                 <span>
-                <p style={{marginTop:'30px', textAlign:'center'}}>Redes Sociais</p>
-                <ul className='menu-redes'> 
-              <li><img src={facebook} alt="" /></li>
-              <li><img src={tiktok} alt="" /></li>
-              <li><img src={instagram} alt="" /></li>
-              <li><img src={whatsapp} alt="" /></li>
-            </ul>
+                    <p style={{ marginTop: '30px', textAlign: 'center' }}>Redes Sociais</p>
+                    <ul className='menu-redes'>
+                        <li><img src={facebook} alt="" /></li>
+                        <li><img src={tiktok} alt="" /></li>
+                        <li><img src={instagram} alt="" /></li>
+                        <li><img src={whatsapp} alt="" /></li>
+                    </ul>
 
                 </span>
-               
-                
+
+
             </section>
             <section className='menu-footer'>
                 <article>
-                    <h1 style={{marginBottom:'30px'}}>Sobre</h1>
-                    <p style={{marginBottom:'45px'}}>A Binho Construções atua na grande Florianópolis desde 2007 executando projetos e serviços que vão desde o alicerce ao acabamento. 
-                        Nossos profissionais estão preparados para executar serviços técnicos e sofisticados de instalações e execução de obras de todos os padrões. 
+                    <h1 style={{ marginBottom: '30px' }}>Sobre</h1>
+                    <p style={{ marginBottom: '45px' }}>A Binho Construções atua na grande Florianópolis desde 2007 executando projetos e serviços que vão desde o alicerce ao acabamento.
+                        Nossos profissionais estão preparados para executar serviços técnicos e sofisticados de instalações e execução de obras de todos os padrões.
                         Nosso compromisso como empresa se baseia na entrega rápida e no baixo custo, priorizando as normas técnicas, a conduta profissional e principalemente, a satisfação do cliente.  </p>
-                
-                <span>
-                <p>Telefone: (48) 9997-3102</p>
-                <p>E-mail: binhoconstrucoes2007@gmail.com</p>
-                <p>Endereço: Rua Valdemiro Eli 800, Potecas-São José-SC</p>
-                </span>
 
+                    <span>
+                        <span><img src= {telefone} alt="" /><p> (48) 9997-3102</p></span>
+                        <span><img src={gmail} alt="" /> <p> binhoconstrucoes2007@gmail.com</p></span>
+                        <span><img src={localizacao} alt="" /> <p> Rua Valdemiro Eli 800, Potecas-São José-SC</p></span>
+                        
+                        
+                        
+                    </span>
                 </article>
-                
+
             </section>
-            <a style={{zIndex:'6'}} className='animate__animated animate__pulse animate__infinite' href={wpmode} target='_blank'></a>
+            <a style={{ zIndex: '6' }} className='animate__animated animate__pulse animate__infinite' href={wpmode} target='_blank'></a>
 
 
 
--- src/styled/icons/Icon.jsx
+++ src/styled/icons/Icon.jsx
@@ -9,16 +9,20 @@ export default styled.div`
   align-items:center;
   width:80px;
   height:80px;
-  border:2px solid ${props=>props.cor};
   border:1px solid rgba(255, 255, 255, 0.898);
-/* box-shadow: 0px 0px 8px 3px rgba(255,255,255,.4); */
-/* box-shadow: 0px 0px 16px 4px rgba(255, 165, 0, 0.631), inset 0px 0px 5px 2px rgba(200, 200, 200, 0.6); */
+  
+  /* box-shadow: 0px 0px 8px 3px rgba(255,255,255,.4); */
+  /* box-shadow: 0px 0px 16px 4px rgba(255, 165, 0, 0.631), inset 0px 0px 5px 2px rgba(200, 200, 200, 0.6); */
   border-radius:50%; 
   ${props=> props.item?"box-shadow: 0px 0px 16px 4px rgba(255, 165, 0, 0.631), inset 0px 0px 5px 2px rgba(200, 200, 200, 0.6);":''
   }
   
   background-image:url${props=>props.icon};
   color:white;
+  &:hover{
+    filter:drop-shadow(-0px 0px 5px  rgb(170, 170, 170)) drop-shadow(0px 0px 5px rgb(170, 170, 170));
+
+  }
 
   `
 
