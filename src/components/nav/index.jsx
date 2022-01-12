1ad558befd2dad70175b2e2ca84b5f60904e5f71
devmayke <74163797+devmayke@users.noreply.github.com>
2022-01-12 17:27:22 -0300

    Menu mobile fecha quando clicado se for o mesmo link da página

--- src/components/nav/index.jsx
+++ src/components/nav/index.jsx
@@ -1,5 +1,6 @@
 import './nav.css' 
 import { Link } from 'react-router-dom'
+
 import logo from './Captura-de-tela-2021-12-23-152415.svg'
 import facebook from './facebook.png'
 import tiktok from './tik-tok.png'
@@ -8,21 +9,34 @@ import whatsapp from './whatsapp.png'
 import { useState, useRef } from 'react'
 export default function Nav(){
     const [menuMobile, setMenuMobile] = useState({ width:'0%', opacity:'0', background:'rgba(0,0,0, .7)', border:'1px solid white', display:'none'})
-  
+    const [isChecked, setIsChecked] = useState(false)
     const menuChecked = useRef()
     function checkMenu() {
   
       if (menuChecked.current.checked) {
         setMenuMobile({ width:'101%', opacity:'1', opacity2:'0', background:'rgba(0,0,0, 0)', border:'none', display:'flex'})
+        setIsChecked(true)
       } else {
         setMenuMobile({ width:'0%', opacity:'0', opacity2:'1',background:'rgba(0,0,0, .7)', border:'1px solid white', display:'none'})
+        setIsChecked(false)
       }
     }
+    function closeMenu(e){
+      console.log(window.location.pathname)
+      
+      if(window.location.pathname == '/' + e.target.href.split('/')[e.target.href.split('/').length -1]){
+        setIsChecked(false)
+        setMenuMobile({ width:'0%', opacity:'0', opacity2:'1',background:'rgba(0,0,0, .7)', border:'1px solid white', display:'none'})
+
+      }
+    }
+
+
   
     return (
         <>
          <nav style={{ background: menuMobile.background, borderBottom: menuMobile.border }} >
-            <div className='logo' style={{ opacity: menuMobile.opacity2 }}><img src={logo} alt="logo Binho construções" /><p>Binho Construções</p></div>
+            <Link to="/" className='logo' style={{ opacity: menuMobile.opacity2 }}><img src={logo} alt="logo Binho construções" /><p>Binho Construções</p></Link>
             <ul style={{ opacity: menuChecked.opacity1 }} className='menu'>
                 <li><Link to='/servicos'>Serviços</Link></li>
                 <li><Link to='/sobre'>Sobre</Link></li>
@@ -31,7 +45,7 @@ export default function Nav(){
             </ul>
             <label htmlFor='check'>
                 <div className='hamburger'>
-                    <input ref={menuChecked} onChange={() => { checkMenu() }} id='check' type="checkbox" />
+                    <input ref={menuChecked} checked={isChecked} onChange={() =>   { checkMenu() }} id='check' type="checkbox" />
                     <div className='t1'></div>
                     <div className='t2'></div>
                     <div className='t3'></div>
@@ -41,7 +55,7 @@ export default function Nav(){
         <div style={{ width:menuMobile.width, opacity:menuMobile.opacity, display:menuMobile.display}} className='menu-mobile'>
       <nav >
         
-        <ul className='menu-vertical'>
+        <ul onClick={(e)=>{closeMenu(e)}} className='menu-vertical'>
           <li style={{ marginBottom:'20px'}} ><span className='logo-mobile' style={{margin:'auto',display:'flex',flexDirection:'column', alignItems:'center', justifyContent:'center'}}><img src={logo} alt="logo Binho construções" /></span><p style={{padding:'10px',fontSize:'20px'}}>Binho Construções</p></li>
           <li><Link to='/servicos'>Serviços</Link></li>
           <li><Link to='/sobre'>Sobre</Link></li>
