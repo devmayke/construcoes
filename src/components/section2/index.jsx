82b84d4061df84f78993ea65ac6e924bfb37444f
devmayke <74163797+devmayke@users.noreply.github.com>
2022-01-17 16:56:18 -0300

    Teste de desempenhon sem scrollanimation

--- src/components/section2/index.jsx
+++ src/components/section2/index.jsx
@@ -34,18 +34,19 @@ export default function Section2() {
     return (
         <section className='section2'>
             <div className="group1">
-                <ScrollAnimation className='article scroll-animation' animateIn="animate__zoomIn"  >
-                    <article >
-                        <h1 class="" >Título</h1>
+                {/* <ScrollAnimation className='article scroll-animation' animateIn="animate__zoomIn"  > */}
+                    <article className='article' >
+                        <h1 class=",
+                        " >Título</h1>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pellentesque dapibus purus eget viverra. Quisque non mollis orci, at vehicula dolor. Morbi orci dolor, elementum at felis ut, efficitur tempus leo. Sed eu diam leo.</p>
                     </article>
-                </ScrollAnimation>
-                <ScrollAnimation className='scroll-animation article ' animateIn="animate__zoomIn">
-                    <article >
+                {/* </ScrollAnimation> */}
+                {/* <ScrollAnimation className='scroll-animation article ' animateIn="animate__zoomIn"> */}
+                    <article className='article' >
                         <h1 class="">Título</h1>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pellentesque dapibus purus eget viverra. Quisque non mollis orci, at vehicula dolor. Morbi orci dolor, elementum at felis ut, efficitur tempus leo. Sed eu diam leo.</p>
                     </article>
-                </ScrollAnimation>
+                {/* </ScrollAnimation> */}
 
             </div>
             <div className='group2'>
