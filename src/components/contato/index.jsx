81d4461acd134863a1b8a2232f56b7de235bed45
Mayke Ken <devmayke@gmail.com>
2022-01-22 12:48:51 -0300

    ajustes dos links das redes sociaias

--- src/components/contato/index.jsx
+++ src/components/contato/index.jsx
@@ -7,40 +7,40 @@ import pintura from './assets/rolo-de-pintura.png'
 import camera from './assets/camera-de-seguranca.png'
 
 export default () => {
-    function linkToServico(link){
+    function linkToServico(link) {
         window.location.assign(`/servicos#${link}`)
 
     }
- 
+
     return (
         <section className='section3'>
             <span>
-            <h1>Serviços</h1>
-            <article className='servicos'>
-                 <div className='servicos-item'><div onClick={()=>{linkToServico('alvenaria')}} className='circle'><img src={alvenaria} alt="" /></div><h1 className='servicos-nome'>Alvenaria</h1></div>
-                 <div className='servicos-item'><div onClick={()=>{linkToServico('carpintaria')}} className='circle'><img src={carpintaria} alt="" /></div><h1 className='servicos-nome'>Carpintaria</h1></div>
-                 <div className='servicos-item'><div onClick={()=>{linkToServico('eletrica')}} className='circle'><img src={eletrica } alt="" /></div><h1 className='servicos-nome'>Elétrica</h1></div>
-                 <div className='servicos-item'><div onClick={()=>{linkToServico('hidraulica')}} className='circle'><img src={hidraulica} alt="" /></div><h1 className='servicos-nome'>Hidráulica</h1></div>
-                 <div className='servicos-item'><div onClick={()=>{linkToServico('pintura')}} className='circle'><img src={pintura} alt="" /></div><h1 className='servicos-nome'>Pintura</h1></div>
-                 <div className='servicos-item'><div onClick={()=>{linkToServico('instalacoes')}} className='circle'><img src={camera} alt="" /></div><h1 className='servicos-nome'>Instalações</h1></div>
-            </article>
-            </span>      
+                <h1>Serviços</h1>
+                <article className='servicos'>
+                    <div className='servicos-item'><div onClick={() => { linkToServico('alvenaria') }} className='circle'><img src={alvenaria} alt="" /></div><h1 className='servicos-nome'>Alvenaria</h1></div>
+                    <div className='servicos-item'><div onClick={() => { linkToServico('carpintaria') }} className='circle'><img src={carpintaria} alt="" /></div><h1 className='servicos-nome'>Carpintaria</h1></div>
+                    <div className='servicos-item'><div onClick={() => { linkToServico('eletrica') }} className='circle'><img src={eletrica} alt="" /></div><h1 className='servicos-nome'>Elétrica</h1></div>
+                    <div className='servicos-item'><div onClick={() => { linkToServico('hidraulica') }} className='circle'><img src={hidraulica} alt="" /></div><h1 className='servicos-nome'>Hidráulica</h1></div>
+                    <div className='servicos-item'><div onClick={() => { linkToServico('pintura') }} className='circle'><img src={pintura} alt="" /></div><h1 className='servicos-nome'>Pintura</h1></div>
+                    <div className='servicos-item'><div onClick={() => { linkToServico('instalacoes') }} className='circle'><img src={camera} alt="" /></div><h1 className='servicos-nome'>Instalações</h1></div>
+                </article>
+            </span>
             <span>
-            <h1 >Deixe sua mensagem</h1>
-            <form  className="contato" action="https://formsubmit.co/devmayke@gmail.com" method='POST'>
+                <h1 >Deixe sua mensagem</h1>
+                <form className="contato" action="https://formsubmit.co/binhoconstrucoes2007@gmail.com" method='POST'>
                     <label htmlFor="nome">Nome</label>
-                    <input id='nome' name="name"  type="text" placeholder='Digite seu nome' required/>
+                    <input id='nome' name="name" type="text" placeholder='Digite seu nome' required />
                     <label htmlFor="email">Email</label>
-                    <input type="email" name="email" placeholder="Digite seu email"/>
+                    <input type="email" name="email" placeholder="Digite seu email" />
                     <label htmlFor="phone">Celular</label>
-                    <input type="number" id="phone" name="phone" placeholder='Digite seu celular com DDD' required/> 
+                    <input type="number" id="phone" name="phone" placeholder='Digite seu celular com DDD' required />
                     <input type="hidden" name="_captcha" value="false"></input>
-                    <input type="hidden" name="_autoresponse" value="Olá, recebi seu email, assim que possível entrarei em contato. Obrigado!"/>
-                    <input type="hidden" name="_next" value='https://binho-construcoes.herokuapp.com/enviado'></input>
-                <label htmlFor="mensagem">Mensagem</label>
-                <textarea id="mensagem" name="message" id="" cols="30" rows="10" placeholder='Digite sua mensagem' required></textarea>
-                <button  type='submit'>Enviar</button>
-            </form>
+                    <input type="hidden" name="_autoresponse" value="Olá, recebi seu email, assim que possível entrarei em contato. Obrigado!" />
+                    <input type="hidden" name="_next" value='https://binhoconstrucoes.com.br/enviado'></input>
+                    <label htmlFor="mensagem">Mensagem</label>
+                    <textarea id="mensagem" name="message" id="" cols="30" rows="10" placeholder='Digite sua mensagem' required></textarea>
+                    <button type='submit'>Enviar</button>
+                </form>
             </span>
         </section>
     )
