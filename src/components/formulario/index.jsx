c2e953a39b1b8366c81118db0ab0e4bd5f4b3960
Mayke Ken <devmayke@gmail.com>
2022-02-08 15:02:46 -0300

    Varios ajustes de responsividade e correçao de bugs

--- src/components/formulario/index.jsx
+++ src/components/formulario/index.jsx
@@ -5,7 +5,7 @@ import { useEffect } from 'react'
 export default function Formulario () {
 
     return (
-        <div style={{width:"100%"}} >
+        <div  className="contato" style={{width:"100%"}} >
             <h1 >Deixe sua mensagem</h1>
             <form  action="https://formsubmit.co/binhoconstrucoes2007@gmail.com" method='POST'>
                 <label htmlFor="nome">Nome</label>
